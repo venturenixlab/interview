9ec4e80a2c677afa8a0a8b32ec0d536fb5367c85
vincent.lau <vincent.lau@venturenix.com>
2022-12-04 22:00:00 +0800

    update

---
+++ BankingSystem/src/main/java/banking/ConsumerAccount.java
@@ -0,0 +1,9 @@
+package banking;
+
+public class ConsumerAccount {
+
+	public ConsumerAccount(Person person, Long accountNumber, int pin, double currentBalance) {
+		// complete the function
+	}
+
+}
